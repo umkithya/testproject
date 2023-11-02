10c47f1f7228518fe25c8007ad341b76166a717d
phatmenghor <menghor.phat@amkcambodia.com>
2023-11-02 20:55:05 +0700

    tets

--- lib/test_page.dart
+++ lib/test_page.dart
@@ -6,7 +6,7 @@ class TestPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("AppBar")),
+      appBar: AppBar(title: Text("test page screen1")),
     );
   }
 }
